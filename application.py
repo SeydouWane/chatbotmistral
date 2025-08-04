6f0fadc1185004050489e5efaca20906abe46e7f
Papa Seydou WANE <129470066+SeydouWane@users.noreply.github.com>
2025-08-04 14:07:32 +0000

    Update application.py

--- application.py
+++ application.py
@@ -73,5 +73,5 @@ def home():
     return send_file("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host=127.0.0.1, port=port)
